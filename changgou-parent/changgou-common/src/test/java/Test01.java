b2238e2a29f693f1c7b4d27346c1f1d5a6e7f906
cjj <591065441@qq.com>
2022-06-19 14:34:40 +0800

    修改了测试文件

--- changgou-parent/changgou-common/src/test/java/Test01.java
+++ changgou-parent/changgou-common/src/test/java/Test01.java
@@ -4,6 +4,8 @@
 
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println("git本地文件的推送练习");
+        System.out.println("git本地文件的推送练习1");
+        System.out.println("git本地文件的推送练习2");
+        System.out.println("git本地文件的推送练习3");
     }
 }
